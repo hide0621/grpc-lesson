7cc51e374daf29b3cd5b96051dd6b5cbc257d076
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-20 08:32:08 +0900

    クライアントストリーミングPRCのサーバーの実装

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"grpc-lesson/pb"
@@ -81,6 +82,29 @@ func (*server) Download(req *pb.DownLoadRequest, stream pb.FileService_DownLoadS
 		time.Sleep(1 * time.Second)
 	}
 	return nil
+}
+
+func (*server) Upload(stream pb.FileService_UploadServer) error {
+	fmt.Println("Upload was invoked")
+
+	var buf bytes.Buffer
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			res := &pb.UploadResponse{Size: int32(buf.Len())}
+			return stream.SendAndClose(res)
+		}
+		if err != nil {
+			return err
+		}
+
+		data := req.GetData()
+		log.Printf("received data(bytes): %v", data)
+		log.Printf("received data(string): %v", string(data))
+		buf.Write(data)
+	}
+
 }
 
 func main() {
