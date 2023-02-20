1af2ae22f9585a23b40add8a47e1665962e7df32
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-21 08:43:14 +0900

    クライアントストリーミングRPCのクライアントの実装

--- client/main.go
+++ client/main.go
@@ -6,6 +6,8 @@ import (
 	"grpc-lesson/pb"
 	"io"
 	"log"
+	"os"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -22,7 +24,8 @@ func main() {
 	client := pb.NewFileServiceClient(conn)
 
 	// callListFiles(client)
-	callDownload(client)
+	// callDownload(client)
+	CallUpload(client)
 }
 
 func callListFiles(client pb.FileServiceClient) {
@@ -50,4 +53,46 @@ func callDownload(client pb.FileServiceClient) {
 		log.Printf("Response from Download(bytes): %v", res.GetDate())
 		log.Printf("Response from Download(string): %v", string(res.GetDate()))
 	}
+}
+
+func CallUpload(client pb.FileServiceClient) {
+	filename := "sports.txt"
+	path := "/Users/fujiwarahideyuki/project/grpc-lesson/strage/" + filename
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	stream, err := client.Upload(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	buf := make([]byte, 5)
+	for {
+		n, err := file.Read(buf)
+		if n == 0 || err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		req := &pb.UploadRequest{Data: buf[:n]}
+		sendErr := stream.Send(req)
+		if sendErr != nil {
+			log.Fatalln(sendErr)
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("received data size: %v", res.GetSize())
 }
